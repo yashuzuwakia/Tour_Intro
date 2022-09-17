f76b981cdc6e6bd5a8b29f19515e9713874b31c0
yashuzuwakia <78296488+yashuzuwakia@users.noreply.github.com>
2022-09-17 08:51:41 +0530

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import Tours from './Tours';
+import Loading from './Loading';
+
 import "./style.css";
 
 export default function App() {
+
+  const [loading,setLoading] = useState(true);
+  const [tours,setTours] = useState([]);
+  if(loading){
   return (
-    <div>
-      <h1>Tour Project Setup</h1>
-    </div>
+    <main>
+      <Loading/>
+    </main>
   );
+}
+return (
+  <main>
+    <Tours/>
+  </main>
+)
+
 }
---
+++ src/Tours.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Tours() {
+  return(
+    <>
+      <h3>Tours</h3>
+    </>
+  )
+}
