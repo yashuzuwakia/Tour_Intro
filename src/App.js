00054bdbc9045785e82ee8d006123cca4b921afe
yashuzuwakia <78296488+yashuzuwakia@users.noreply.github.com>
2022-09-17 09:02:17 +0530

    try and catch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import React,{useState,useEffect} from "react";
 import Tours from './Tours';
 import Loading from './Loading';
 
+const url = 'https://course-api.com/react-tours-project';
+
 import "./style.css";
 
 export default function App() {
 
   const [loading,setLoading] = useState(true);
   const [tours,setTours] = useState([]);
+
+  const fetchTours =  async () => {
+    setLoading(true);
+
+    try{
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+        setLoading(false);
+    }
+    
+    console.log(tours);
+   }
+
+   useEffect(()=>{
+      fetchTours();
+   },[] )
+
   if(loading){
   return (
     <main>
